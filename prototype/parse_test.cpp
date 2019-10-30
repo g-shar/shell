3606e85b476ad2bc0d7fd193ae83f46d9ba2fe44
Richard Duong <rduon008@ucr.edu>
2019-10-29 19:53:05 -0700

    rough attempt at parsing

--- prototype/parse_test.cpp
+++ prototype/parse_test.cpp
@@ -10,18 +10,69 @@ int main(){
 	char* test5 = "ls;";
 	char* test6 = "ls -a; echo hello && mkdir test || echo world; git status;";
 	char* test7 = "cd";
+	char* test8 = "";
 
 }
 
-char* handleQuotes(const char* phrase){
-
+char* checkSpace(const char* phrase){
+	if(*phrase != " ")
+	{
+		return NULL;
+	}
+	return phrase;
 }
+
+char* checkSemicolon(const char* phrase){
+	if(*phrase != ";")
+	{
+		return NULL;
+	}
+	return phrase;
+}
+
+char* checkAnd(const char* phrase){
+	if(phrase[0] != "&" && phrase[1] != "&")
+	{
+		return NULL;
+	}
+
+	// skips forward?
+	return phrase + 1;
+}
+
+char* checkOr(const char* phrase){
+	if(phrase[0] != "|" && phrase[1] != "|")
+	{
+		return NULL;
+	}	
+	return phrase + 1;
+}
+
+char* checkQuotes(const char* phrase){
+	if(phrase[0] == "\"")
+	{
+		for(int i = 1; phrase[i] != "\0"; ++i){
+			if(phrase[i] == "\"")
+			{
+				return phrase + i;
+			}
+		}
+		
+		//	error from unfinished quote
+		exit(1);
+	}
+
+	return NULL;
+}
+
 
 
 
 void parseDisplay(const char* phrase){
+	
+
 	for(int i = 0; phrase[i] != "\0"; ++i){
-		
+				
 	}
 
 }
