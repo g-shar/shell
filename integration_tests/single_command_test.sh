4c46ebdc4dec57454a871f44ea00cdf0eb653d6b
Gregory Shar <gregory.shar75@gmail.com>
2019-11-13 14:07:56 -0800

    Initial single command test script

--- integration_tests/single_command_test.sh
+++ integration_tests/single_command_test.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
-echo "single_command_tests.sh"
+INPUTS=( "echo \"hello\""  "ls -a" "mkdir test")
+
+for input in "${INPUTS[@]}"
+do
+   echo "SingleCommandExecutions test with ${input}"
+   ./rshell ${input} > integration_tests
+   EXECUTION=$?
+   if [ $EXECUTION -eq 0 ];
+   then
+      echo "A command was executed" 
+   else
+      echo "${input} failed and was not executed"
+      exit 1
+   fi
+   
+   input1=$(${input})
+   echo "Checking if correct execution of command..."
+   if [ "$output" == "$input1" ]
+   then 
+      echo "Single Command Execution test passed with ${input}"
+   else 
+      echo "Single Command Execution test failed with ${input}"
+   fi
+done
 
 #tests primarily for command executions
