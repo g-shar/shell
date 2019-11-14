187a2e61f7a151447f13ca4ece642c9f876a09fa
ucrRichard <rduon008@ucr.edu>
2019-11-13 19:52:31 -0800

    Finished cmd_obj

--- header/cmd_obj.hpp
+++ header/cmd_obj.hpp
@@ -5,16 +5,46 @@
 
 class Cmd_Obj: public Base_Cmd {
 public:
-   Cmd_Obj(char* cmd, char* list[]): executable(cmd), argList(list){};
-   virtual bool doWork();
+	Cmd_Obj(char* cmd){
+		parse(cmd);		
+	}
+
+	Cmd_Obj(char* cmd, char* list[]): executable(cmd), argList(list){};
+	virtual bool doWork();
 
 private:
-   void parse(){
 
-   }
+	void parse(char* cmd){
+		int i = 0;			// Counter for the command passed in
+		int j = 0;			// Counter for the cstr arr
+		int length = 0;		// For length determination
 
-   char* executable;
-   char** argList;
+		while(cmd[i] != '\0')
+		{
+			if(checkQuotes(cmd) != NULL)
+			{
+				length = sizeQuote(phrase + i);	
+			}			
+
+			else if(checkSpace(cmd) != NULL)
+			{
+				++i;
+				continue;
+			}
+
+			else{
+				length = sizeArg(phrase + i);
+			}
+
+			argList[j] = newStrCpy(phrase + i, length);
+			i += length;
+			++j;
+		}
+		cmd = argList[0];
+  	}
+
+   	char* executable;
+   	char** argList;
 };
 
 #endif
