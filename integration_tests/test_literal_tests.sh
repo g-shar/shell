dd6831517b0cdb641da0cd9ddf792a9cf3d13563
Gregory Shar <gregory.shar75@gmail.com>
2019-11-27 20:00:09 -0800

    Update test command integration tests

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -1,44 +1,61 @@
 #!/bin/sh
 
 TESTS=("test -e ../unit_test" "test -f ../names.txt" "test -d ../integration_test")
-TESTS2="echo hello"
+
 for input in "${TESTS[@]}"
 do
-   echo "Testing with ${input} && echo \"executed\""
+   echo "Testing connectors with \"${input}\""
    echo " "
-   rshell_result= $(../rshell {input})  &&  echo "executed" > integration_tests.txt 
+   rshell_result= $(../rshell {input})  &&  echo "2nd_cmd_executed" > integration_tests.txt 
    system_result=$(${input})	#runs the test command
    if [ "$?" -eq 0 ];	#if test  command is successful, then next command should be executed
    then
-      system_result= "executed" 
+      system_result="2nd_cmd_executed" 
    fi
-   output=$(grep ^$system_result integration_tests.txt)
+   output=$(grep ^${system_result} integration_tests.txt)
    echo "system: ${system_result}"
-   echo "rshell: ${rshell_result}"
+   echo "rshell: ${output}"
    if [ ! -z "$output" ]
    then 
-      	echo "Test command execution test passed with ${input}"
+      	echo "TestCMD Test passed with \"${input} && echo 2nd_cmd_executed\""
    else 
-      	echo "Test command execution test failed with ${input}"
+      	echo "TestCMD Test failed with \"${input} && echo 2nd_cmd_executed\""
    fi
+   echo " "
 
    echo "Testing with || connectors..."
-   rshell_result=$(../rshell ${input}) || echo "executed" > integration_tests.txt 
+   rshell_result=$(../rshell ${input}) || echo "2nd_cmd_executed" > integration_tests.txt 
    system_result=$(${input})		#runs the test command
    if [ ! "$?" -eq 0 ];	#if test command is not successful, then next command should be executed
    then 
-      system_result="executed"
+      system_result="2nd_cmd_executed"
    fi
    output=$(grep ^$system_result integration_tests.txt)
    echo "system: ${system_result}"
-   echo "rshell: ${rshell_result}"
+   echo "rshell: ${output}"
    echo "Checking if correct execution of command..."
    if [ ! -z "$output" ]
    then 
-      	echo "Test command execution test passed with ${input}"
+      	echo "TestCMD Test passed with \"${input} || echo 2nd_cmd_executed\""
    else 
-      	echo "Test command execution test failed with ${input}"
-   fi 
+      	echo "TestCMD Test failed with \"${input} || echo 2nd_cmd_executed\""
+   fi
+   echo " "
+
+   echo "Testing with ; connectors..."
+   rshell_result=$(../rshell ${input}) ; echo "2nd_cmd_executed" > integration_tests.txt 
+   system_result="2nd_cmd_executed"
+   output=$(grep ^$system_result integration_tests.txt)
+   echo "system: ${system_result}"
+   echo "rshell: ${output}"
+   echo "Checking if correct execution of command..."
+   if [ ! -z "$output" ]
+   then 
+      	echo "TestCMD test passed with \"${input} ; echo 2nd_cmd_executed\""
+   else 
+      	echo "TestCMD test failed with \"${input} ; echo 2nd_cmd_executed\""
+   fi
+   echo " "
 done
 
 
