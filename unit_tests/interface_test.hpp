638e9f839b5493df97652824b7ae941204854510
Gregory Shar <gregory.shar75@gmail.com>
2019-11-27 16:43:15 -0800

    Fix interface unit tests

--- unit_tests/interface_test.hpp
+++ unit_tests/interface_test.hpp
@@ -32,7 +32,7 @@ TEST(InterfaceTestSet, multiCommands){
 	char* arg1 = "echo hello && echo goodbye";
 	char* arg2 = "ech whoa || echo passed";
 	char* arg3 = "not yes || echo why";
-	char* arg4 = "ec all || ec no || echo yes"; 
+	char* arg4 = "ec all || ec no && echo yes"; 
 
 
 
@@ -43,7 +43,7 @@ TEST(InterfaceTestSet, multiCommands){
 	EXPECT_EQ(test.doWork(), true);
 
 	test.setCommand(arg3);
-	EXPECT_EQ(test.doWork(), false);
+	EXPECT_EQ(test.doWork(), true);
 
 	test.setCommand(arg4);
 	EXPECT_EQ(test.doWork(), false);
