c7ddc680e1805d8f86fbfa47323ad2af5e3969f1
Gregory Shar <gregory.shar75@gmail.com>
2019-12-13 13:15:23 -0800

    Updated unit tests

--- unit_tests/io_cmd_test.hpp
+++ unit_tests/io_cmd_test.hpp
@@ -1,11 +1,15 @@
 #ifndef __TEST_CMD_TEST_HPP__
 #define __TEST_CMD_TEST_HPP__
 
+#include <cstring>
 #include <fstream>
 #include "../header/cmd_obj.hpp"
+#include "../header/interface.hpp"
 
 TEST(IOCmdTestSet, OutputOperationTest) {
-   char* fullCmd="echo hello < input.txt";
+   string cmd="echo hello > input.txt";
+   char fullCmd[cmd.size()+1];
+   strcpy(fullCmd, cmd.c_str());
    Interface* object1= new Interface(fullCmd);
    EXPECT_EQ(object1->doWork(), true);
 
@@ -13,12 +17,12 @@ TEST(IOCmdTestSet, OutputOperationTest) {
 
 TEST(IOCmdTestSet, OutputAppendTest) {
    char* argList="echo appended >> input.txt";
-   Interface* object1= new Interface(fullCmd);
+   Interface* object1= new Interface(argList);
    
    bool result=object1->doWork();
    ifstream inFS;
    inFS.open("input.txt");
-   string x="":
+   string x="";
    inFS>>x;
 
    EXPECT_EQ(result, true);
