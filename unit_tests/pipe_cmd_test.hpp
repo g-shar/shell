ae70b7de474b05fc5a07bef7f89c8484c8875a1b
Gregory Shar <gregory.shar75@gmail.com>
2019-12-13 16:53:54 -0800

    Pipe command test update

--- unit_tests/pipe_cmd_test.hpp
+++ unit_tests/pipe_cmd_test.hpp
@@ -1,40 +1,34 @@
-#ifndef __TEST_CMD_TEST_HPP__
-#define __TEST_CMD_TEST_HPP__
+#ifndef __PIPE_CMD_TEST_HPP__
+#define __PIPE_CMD_TEST_HPP__
 
 #include <fstream>
 #include "../header/cmd_obj.hpp"
 
-TEST(IOCmdTestSet, OutputOperationTest) {
-   char* fullCmd="echo hello < input.txt";
+TEST(IOCmdTestSet, onePipeTest) {
+   char* fullCmd="echo hello | tr a-z A-Z";
    Interface* object1= new Interface(fullCmd);
    EXPECT_EQ(object1->doWork(), true);
 
 }
 
-TEST(IOCmdTestSet, OutputAppendTest) {
-   char* argList="echo appended >> input.txt";
+TEST(IOCmdTestSet, twoPipeTest) {
+   char* argList="echo APPENDED | tr A-Z a-z | tr a-z A-Z";
    Interface* object1= new Interface(fullCmd);
    
    bool result=object1->doWork();
-   ifstream inFS;
-   inFS.open("input.txt");
-   string x="":
-   inFS>>x;
-
    EXPECT_EQ(result, true);
-   EXPECT_EQ(x, "appended");
 
 }
 
-TEST(IOCmdTestSet, InputTest) {
-   char* argList="cat < input.txt";
+TEST(IOCmdTestSet, threePipeTest) {
+   char* argList="echo thirdTry | tr a-z A-Z | tr A-Z a-z | tr a-z A-Z";
    Interface* object1= new Interface(argList);
 
    EXPECT_EQ(object1->doWork(), true);
 }
 
 TEST(IOCmdTestSet, ErrorTest) {
-   char* argList="eho error > input.txt";
+   char* argList="eho error | tr a-z";
 
    Interface* object1= new Interface(argList);
 
